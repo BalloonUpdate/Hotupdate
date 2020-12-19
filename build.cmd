96c193cc631311ef6213767a1356bbbff35924f6
innc11 <pm@innc11.cn>
2020-12-19 19:53:26 +0800

    style(icon): 增加了图标

--- build.cmd
+++ build.cmd
@@ -1,5 +1,7 @@
 @echo off
-pyinstaller --noconfirm --version-file version-file.txt -w -n main main.py
+pyinstaller --noconfirm --version-file version-file.txt -i icon.ico -w -n main main.py
 echo Build finished!
 rmdir /S /Q D:\nginx-1.19.1\html\hotupdate\
 xcopy /E /R /Y dist\main\* D:\nginx-1.19.1\html\hotupdate\
+rmdir /S /Q D:\nginx-1.19.1\updatertest\hotupdate\
+xcopy /E /R /Y dist\main\* D:\nginx-1.19.1\updatertest\hotupdate\
