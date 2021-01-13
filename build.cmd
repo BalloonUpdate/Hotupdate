eac8cabe38960e52f912e9511ba3b9c5787d2667
innc11 <pm@innc11.cn>
2021-01-13 16:33:48 +0800

    build: 修改build脚本，输出版本标识文件

--- build.cmd
+++ build.cmd
@@ -2,12 +2,20 @@
 
 python version.py > temp.txt
 set /p filename=< temp.txt
+
+python -c print('%filename%'.split('-')[1]) > temp.txt
+set /p version_text=< temp.txt
+
 del temp.txt
 
+del dist\%version_text%
 echo Build for %filename%
-
 pyinstaller --noconfirm --version-file version-file.txt --add-data="icon.ico;." -i icon.ico -F -w -n UpdaterHotupdatePackage main.py %1 %2 %3
 echo Build finished!
+
+echo %filename% > dist\%version_text%
+echo %version_text%
+
 
 rmdir /S /Q                         D:\nginx-1.19.1\updater-php\hotupdate\
 xcopy /E /R /Y dist\*               D:\nginx-1.19.1\updater-php\hotupdate\
